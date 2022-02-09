5281661078e44d89d0d0ca9bd8f61c07d8197953
Ashen-den <97136128+Ashen-den@users.noreply.github.com>
2022-02-08 21:45:38 -0800

    Completed Training JS#31

---
+++ 8 Kyu/Training_js_31.js
@@ -0,0 +1,7 @@
+function blackAndWhite(arr){
+  if (Array.isArray(arr) === true) {
+    if (arr.indexOf(5) != -1 && arr.indexOf(13) != -1) {
+      return "It's a black array" 
+    } else {return "It's a white array"}
+    } else {return "It's a fake array"}
+}
