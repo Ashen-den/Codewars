0c3845880133e7e242f8b6107de880a810b278f8
Ashen-den <97136128+Ashen-den@users.noreply.github.com>
2022-02-18 21:52:08 -0800

    Completed is it even

---
+++ 8 Kyu/Is_it_even.js
@@ -0,0 +1,14 @@
+function testEven(n) {
+  if (typeof n === "number") {
+   
+    if (n === 0) {
+      return true
+    } else if (n%2 === 0) {
+      return true
+    } else {
+      return false
+    }
+    
+    
+   }
+}
