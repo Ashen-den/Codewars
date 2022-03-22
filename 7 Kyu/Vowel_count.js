556cedb03fb83e9a5fe6d2f627c4523b5f5042bb
Ashen-den <97136128+Ashen-den@users.noreply.github.com>
2022-03-21 23:16:26 -0700

    Completed vowel count

---
+++ 7 Kyu/Vowel_count.js
@@ -0,0 +1,10 @@
+function getCount(str) {
+  let vowelsCount = 0;
+  str.split('').map(e =>{
+    e === 'a' || e === 'e' || e === 'i' || e === 'o' || e === 'u'
+    ? vowelsCount++ : '' ;
+  })
+ 
+  
+  return vowelsCount;
+}
