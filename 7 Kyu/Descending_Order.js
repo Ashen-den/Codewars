16ae497636a9100eb2863c930013fee6ddf11f87
Ashen-den <97136128+Ashen-den@users.noreply.github.com>
2022-05-15 23:58:46 -0700

    terminal testing

--- 7 Kyu/Descending_Order.js
+++ 7 Kyu/Descending_Order.js
@@ -1,4 +1,4 @@
-// Your task is to make a function that can take any non-negative integer as an argument and return it with its digits in descending order. Essentially, rearrange the digits to create the highest possible number.
+// Your task is to make a function that can take any non-negative integer as an argument and return it with its digits in descending order. Essentially, rearrange the digits to create the highest possible number. test
 
 function descendingOrder(n){
   //Turn argument into a string, then an array, sort the array, convert back to integer, join the array and return
