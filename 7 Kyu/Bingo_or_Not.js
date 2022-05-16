9ae2aa78d0a90941e4e7f2477757e686f731ad0a
Ashen-den <97136128+Ashen-den@users.noreply.github.com>
2022-05-15 22:34:37 -0700

    added bingo or not

---
+++ 7 Kyu/Bingo_or_Not.js
@@ -0,0 +1,10 @@
+//Use conditional check for all letters in array
+
+function bingo(a) {
+  if(a.indexOf(2) > -1 && a.indexOf(9) > -1 && a.indexOf(14) > -1 && a.indexOf(7) > -1 && a.indexOf(15) > -1){
+    return "WIN"
+  } else {
+    return "LOSE"
+  }
+  
+}
