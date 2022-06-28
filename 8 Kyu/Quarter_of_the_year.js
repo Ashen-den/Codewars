a8b9c1f56053d4f0470023fecaabe97018deb979
Ashen-den <97136128+Ashen-den@users.noreply.github.com>
2022-06-27 17:22:19 -0700

    Completed Quarter of the year

---
+++ 8 Kyu/Quarter_of_the_year.js
@@ -0,0 +1,15 @@
+const quarterOf = (month) => {
+  switch(true){
+      case month<4:
+      return 1
+      break;
+      case month<7:
+      return 2
+      break;
+      case month<10:
+      return 3
+      break;
+      case month<13:
+      return 4
+      break;
+  }
