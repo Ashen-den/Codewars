c5a381e12060355f7f99c7bf935c36f35febdc52
Ashen-den <97136128+Ashen-den@users.noreply.github.com>
2022-07-09 15:05:56 -0700

    Completed Help Suzuki complete his chores!

---
+++ 7 Kyu/Help_Suzuki_complete_his_chores.js
@@ -0,0 +1,8 @@
+function choreAssignment(chores) {
+  let sortChore = chores.sort((a,b)=>a-b)
+  let answer = []
+  for(let i=0;i<chores.length/2;i++){
+    answer.push(sortChore[i]+sortChore[sortChore.length-1-i])
+  }
+  return answer.sort((a,b)=>a-b)
+}
